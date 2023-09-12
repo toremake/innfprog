accfeb61134a1d855874a8629b14fed5adcab8b5
Tore Marius Akerbæk <tore.m.akerbak@hiof.no>
2023-09-12 13:36:38 +0200

    Oppdatert datasets

---
+++ datasets/photogallery.js
@@ -0,0 +1,37 @@
+//All pictures fetched with permission from unsplash.com
+
+const photogallery = [
+    {
+        title: "Moose",
+        photographer: "Santonii",
+        imageURL: "https://images.unsplash.com/photo-1694304804759-7c738ce19586",
+        category: "Nature"
+    },
+    {
+        title: "Cabin",
+        photographer: "Shawnanggg",
+        imageURL: "https://images.unsplash.com/photo-1691848749210-5d06893a53db",
+        category: "Nature"
+    },
+    {
+        title: "Baby",
+        photographer: "Kateryna Hliznitsova",
+        imageURL: "https://images.unsplash.com/photo-1693251802295-0c14bee04607",
+        category: "People"
+    },
+    {
+        title: "Kiss of leaves",
+        photographer: "Jennifer Kalenberg",
+        imageURL: "https://images.unsplash.com/photo-1694231665185-bf22c08d9ced",
+        category: "People"
+    },
+    {
+        title: "The road",
+        photographer: "Pedro Mestres",
+        imageURL: "https://images.unsplash.com/photo-1693773606486-82a749556017",
+        category: "Nature"
+    },
+    
+]
+
+export default photogallery
